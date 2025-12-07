91338b1d8b1637ba75d07b7b989f0e0da2315759
Jason Gorman <jason.gorman@codemanship.com>
2025-12-07 09:37:52 +0000

    Added projects for Comments code smell

---
+++ Comments/MarsRover.cs
@@ -0,0 +1,131 @@
+﻿namespace Comments;
+
+public class MarsRover
+{
+    public string Facing { get; private set; }
+    public int[] Coordinates { get; }
+
+    public MarsRover(string facing, int[] coordinates)
+    {
+        Facing = facing;
+        Coordinates = coordinates;
+    }
+
+    public void Go(string instructions)
+    {
+        foreach (var instruction in instructions.ToCharArray())
+        {
+            Execute(instruction);
+        }
+    }
+
+    private void Execute(char instruction)
+    {
+        // R = Turn Right
+        if (instruction == 'R')
+        {
+            // N = North
+            if (Facing == "N")
+            {
+                // E = East
+                Facing = "E";
+                return;
+            }
+
+            if (Facing == "E")
+            {
+                // S = South
+                Facing = "S";
+                return;
+            }
+
+            if (Facing == "S")
+            {
+                // W = West
+                Facing = "W";
+                return;
+            }
+
+            Facing = "N";
+        }
+
+        // L = Turn Left
+        if (instruction == 'L')
+        {
+            if (Facing == "N")
+            {
+                Facing = "W";
+                return;
+            }
+
+            if (Facing == "W")
+            {
+                Facing = "S";
+                return;
+            }
+
+            if (Facing == "S")
+            {
+                Facing = "E";
+                return;
+            }
+
+            Facing = "N";
+        }
+
+        // F = Move Forward
+        if (instruction == 'F')
+        {
+            if (Facing == "N")
+            {
+                // Y + 1
+                Coordinates[1] = Coordinates[1] + 1;
+            }
+
+            if (Facing == "E")
+            {
+                // X + 1
+                Coordinates[0] = Coordinates[0] + 1;
+            }
+
+            if (Facing == "S")
+            {
+                // Y - 1
+                Coordinates[1] = Coordinates[1] - 1;
+            }
+
+            if (Facing == "W")
+            {
+                // X - 1
+                Coordinates[0] = Coordinates[0] - 1;
+            }
+        }
+
+        if (instruction == 'B')
+        {
+            if (Facing == "N")
+            {
+                // Y - 1
+                Coordinates[1] = Coordinates[1] - 1;
+            }
+
+            if (Facing == "E")
+            {
+                // X - 1
+                Coordinates[0] = Coordinates[0] - 1;
+            }
+
+            if (Facing == "S")
+            {
+                // Y + 1
+                Coordinates[1] = Coordinates[1] + 1;
+            }
+
+            if (Facing == "W")
+            {
+                // X + 1
+                Coordinates[0] = Coordinates[0] + 1;
+            }
+        }
+    }
+}
