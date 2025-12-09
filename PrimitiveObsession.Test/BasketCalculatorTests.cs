a291e2e6498783eb19e98bcaf6e229d49ca6de0f
Jason Gorman <jason.gorman@codemanship.com>
2025-12-09 11:31:19 +0000

    Added Primitive Obsession projects

---
+++ PrimitiveObsession.Test/BasketCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿namespace PrimitiveObsession.Test;
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+[TestFixture]
+public class BasketCalculatorTests
+{
+    [Test]
+    public void CalculateTotal_WithMultipleItems_ReturnsCorrectTotal()
+    {
+        var basket = new List<string[]>
+        {
+            new[] { "Apple", "0.50", "3" },       // 1.50
+            new[] { "Pear", "0.80", "2" },        // 1.60
+            new[] { "Chocolate", "2.00", "1" }    // 2.00
+        };
+
+        decimal total = BasketCalculator.CalculateTotal(basket);
+
+        Assert.That(total, Is.EqualTo(5.10m));
+    }
+
+    [Test]
+    public void CalculateTotal_WithEmptyBasket_ReturnsZero()
+    {
+        var basket = new List<string[]>();
+
+        decimal total = BasketCalculator.CalculateTotal(basket);
+
+        Assert.That(total, Is.EqualTo(0m));
+    }
+
+    [Test]
+    public void CalculateTotal_WithSingleItem_ReturnsPriceTimesQuantity()
+    {
+        var basket = new List<string[]>
+        {
+            new[] { "Banana", "1.20", "4" }   // 4.80
+        };
+
+        decimal total = BasketCalculator.CalculateTotal(basket);
+
+        Assert.That(total, Is.EqualTo(4.80m));
+    }
+}
---
+++ PrimitiveObsession/BasketCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimitiveObsession;
+
+using System;
+using System.Collections.Generic;
+
+public static class BasketCalculator
+{
+    public static decimal CalculateTotal(List<string[]> basket)
+    {
+        decimal total = 0;
+
+        foreach (var item in basket)
+        {
+            decimal price = decimal.Parse(item[1]);
+            int quantity = int.Parse(item[2]);
+
+            total += price * quantity;
+        }
+
+        return total;
+    }
+}
