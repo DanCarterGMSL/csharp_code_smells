091b2fd13b142525719636cfa415e16b87300e3b
Lucas BK <lucas.blackaller-kidd@gmsl.co.uk>
2025-12-10 15:59:52 +0000

    remove unused fields

--- Duplication/Invoice.cs
+++ Duplication/Invoice.cs
@@ -14,21 +14,10 @@ public class Item
 
 public class Invoice
 {
-    private readonly Item? _item1;
-    private readonly Item? _item2;
-    private readonly Item? _item3;
-    private readonly Item? _item4;
-    private readonly Item? _item5;
     private readonly Item?[] _items;
 
     public Invoice(Item? item1, Item? item2, Item? item3, Item? item4, Item? item5, Item?[] items)
     {
-        _item1 = item1;
-        _item2 = item2;
-        _item3 = item3;
-        _item4 = item4;
-        _item5 = item5;
-
         _items = items;
     }
 
