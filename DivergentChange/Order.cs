dbe53db250a447ce745baece20208c30895738a8
Dan Carter <dan.carter@gmsl.co.uk>
2025-12-10 16:28:54 +0000

    extract class for shipping

--- DivergentChange/Order.cs
+++ DivergentChange/Order.cs
@@ -36,21 +36,7 @@ using System.Linq;
         public decimal CalculateShipping(string region)
         {
             decimal total = CalculateTotal();
-            return CalculatingShippingCost(region, total);
-        }
-
-        private static decimal CalculatingShippingCost(string region, decimal total)
-        {
-            decimal shipping;
-
-            if (region == "EU")
-                shipping = total > 100 ? 0 : 10;
-            else if (region == "UK")
-                shipping = total > 50 ? 0 : 8;
-            else
-                shipping = total > 75 ? 0 : 15;
-
-            return shipping;
+            return Shipping.CalculatingShippingCost(region, total);
         }
 
         public List<OrderItem> Items => _items;
