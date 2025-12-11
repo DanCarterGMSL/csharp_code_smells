47adbb7218bcd61b0e7be27f34eb0609df85c932
Dan Carter <dan.carter@gmsl.co.uk>
2025-12-11 12:40:22 +0000

    working

--- FeatureEnvy/Services/ConfirmOrderService.cs
+++ FeatureEnvy/Services/ConfirmOrderService.cs
@@ -5,17 +5,19 @@ namespace FeatureEnvy.Services;
 public class OrderConfirmationService
 {
     private readonly StockService _stockService;
+    private readonly List<WarehouseStock> _stocks;
 
     public OrderConfirmationService(StockService stockService)
     {
         _stockService = stockService;
+        _stocks = _stockService.Stocks;
     }
 
     public bool ConfirmOrder(Order order)
     {
         foreach (var item in order.Items)
         {
-            if (!item.Product.IsAvailable(item.Quantity, _stockService.Stocks))
+            if (!item.Product.IsAvailable(item.Quantity, _stocks))
             {
                 order.Confirmed = false;
                 return false;
