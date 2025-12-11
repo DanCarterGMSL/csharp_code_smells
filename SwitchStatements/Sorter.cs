21c456750d26c5eff9fa17d52c049d84180e02f8
Dan Carter <dan.carter@gmsl.co.uk>
2025-12-11 14:08:04 +0000

    format document

--- SwitchStatements/Sorter.cs
+++ SwitchStatements/Sorter.cs
@@ -11,14 +11,18 @@ public class Sorter
 {
     public int[] Sort(SortKind kind, int[] input)
     {
-        switch(kind){
+        switch (kind)
+        {
             case SortKind.Bubble:
                 bool sorted = false;
-                while(!sorted){
+                while (!sorted)
+                {
                     sorted = true;
-                    for (int i = 0; i < input.Length - 1; i++) {
-                        if(input[i] > input[i+1]){
-                            Swap(input, i, i+1);
+                    for (int i = 0; i < input.Length - 1; i++)
+                    {
+                        if (input[i] > input[i + 1])
+                        {
+                            Swap(input, i, i + 1);
                             sorted = false;
                         }
                     }
@@ -28,10 +32,13 @@ public class Sorter
                 input = Quicksort(input, 0, input.Length - 1);
                 break;
             case SortKind.Insertion:
-                for (int i = 0; i < input.Length - 1; i++) {
-                    for(int j = i+1;j > 0;j--){
-                        if(input[j] < input[j-1]){
-                            Swap(input, j, j-1);
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    for (int j = i + 1; j > 0; j--)
+                    {
+                        if (input[j] < input[j - 1])
+                        {
+                            Swap(input, j, j - 1);
                         }
                     }
                 }
@@ -39,36 +46,42 @@ public class Sorter
         }
         return input;
     }
-    
-    private void Swap(int[] input, int index1, int index2) {
+
+    private void Swap(int[] input, int index1, int index2)
+    {
         int first = input[index1];
         int second = input[index2];
         input[index1] = second;
         input[index2] = first;
     }
 
-    private int[] Quicksort(int[] input, int left, int right) {
+    private int[] Quicksort(int[] input, int left, int right)
+    {
         int i = left, j = right;
         int pivot = input[(left + right) / 2];
         int k = i;
-        while (k <= j) { 
-            while (input[k] < pivot) 
-                k++; 
-            while (input[j] > pivot) 
-                j--; 
-            if (k <= j) { 
+        while (k <= j)
+        {
+            while (input[k] < pivot)
+                k++;
+            while (input[j] > pivot)
+                j--;
+            if (k <= j)
+            {
                 Swap(input, k, j);
                 k++;
                 j--;
-            } 
+            }
         }
 
         i = k;
         int index = i;
-        if (left < index - 1){
+        if (left < index - 1)
+        {
             Quicksort(input, left, index - 1);
         }
-        if (index < right){
+        if (index < right)
+        {
             Quicksort(input, index, right);
         }
         return input;
