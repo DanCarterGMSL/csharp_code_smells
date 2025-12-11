f8b273e7cd2fdad36006e719b6d6393a340a798f
Dan Carter <dan.carter@gmsl.co.uk>
2025-12-11 12:20:38 +0000

    move method

--- FeatureEnvy/Services/BasketService.cs
+++ FeatureEnvy/Services/BasketService.cs
@@ -6,24 +6,6 @@ public class BasketService
 {
     public void AddToBasket(Basket basket, Product product, int qty)
     {
-        AddToBasket2(basket, product, qty);
-    }
-
-    private static void AddToBasket2(Basket basket, Product product, int qty)
-    {
-        var existing = basket.Items.FirstOrDefault(i => i.Product == product);
-
-        if (existing == null)
-        {
-            basket.Items.Add(new Item
-            {
-                Product = product,
-                Quantity = qty
-            });
-        }
-        else
-        {
-            existing.Quantity += qty;
-        }
+        basket.AddToBasket2(product, qty);
     }
 }
